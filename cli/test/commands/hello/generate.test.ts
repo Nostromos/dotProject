141229faeeb53d012aee7c0ffeba9b80a231fdfe
Michael Monaghan <michael@monaghan.nyc>
2025-06-16 14:34:12 -0400

    kill workspace to allow cli/ folder node instance to find proper folders

--- cli/test/commands/hello/generate.test.ts
+++ cli/test/commands/hello/generate.test.ts
@@ -1,8 +1,8 @@
 import { runCommand } from '@oclif/test'
 import { expect } from 'chai'
 
-describe('hello world', () => {
-  it('runs hello world cmd', async () => {
+describe('Generate Command', () => {
+  it('runs generate cmd', async () => {
     const { stdout } = await runCommand('generate')
     expect(stdout).to.contain('INFO.yml generated at')
   })
